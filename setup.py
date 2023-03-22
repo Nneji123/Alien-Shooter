e43330521d9eca1c7438f8701551437f89bdad6c
Nneji123 <ifeanyinneji777@gmail.com>
2023-03-22 11:44:52 +0100

    released exe for windows

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@ from cx_Freeze import setup, Executable
 
 # Include all the required files
 include_files = [
-    "assets/alien.png",
-    "assets/background.png",
-    "assets/bullet.png",
-    "assets/explosion.wav",
-    "assets/laser.mp3",
-    "assets/player.png",
-    "assets/sfx.mp3",
+    "assets",
     "icon.ico",
-    "assets/SPACEBOY.ttf",
 ]
 
 # Dependencies
-build_exe_options = {"packages": ["pygame"], "include_files": include_files}
+build_exe_options = {"packages": ["pygame"], "excludes": ["tkinter"], "include_files": include_files}
 
 # Executable
 base = None
@@ -34,7 +27,7 @@ executable = [
 setup(
     name="Alien Shooter",
     version="1.0",
-    description="A simple game",
+    description="Destroy all aliens!",
     options={"build_exe": build_exe_options},
     executables=executable,
 )
